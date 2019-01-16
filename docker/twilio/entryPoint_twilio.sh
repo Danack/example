eb6f9e786f2463e70c1d18c31d74466d6f111662
Danack <Danack@basereality.com>
2019-01-16 17:01:24 +0000

    Stop action from tailing eternally.

--- docker/twilio/entryPoint_twilio.sh
+++ docker/twilio/entryPoint_twilio.sh
@@ -4,14 +4,15 @@ set -e
 set -x
 
 echo "Hello hello "
-echo "Destinatio is ${TWILIO_DESTINATION} "
+echo "Destination is ${TWILIO_DESTINATION} "
+
+
+set +e
 
 curl -X POST https://api.twilio.com/2010-04-01/Accounts/${TWILIO_SID}/Messages.json \
 --data-urlencode "Body=This is a test message." \
 --data-urlencode "From=${TWILIO_OA}" \
 --data-urlencode "To=${TWILIO_DESTINATION}" \
 -u ${TWILIO_SID}:${TWILIO_TOKEN}
-
-
 
 echo "fin"
